18ed1f56ffbbf2c5099cd74422b0c642ba5f7a36
张帆 <fireworks_zf@163.com>
2017-04-05 13:48:03 +0800

    AppOverlay

--- source/components/appOverlay/AppOverlay.react.js
+++ source/components/appOverlay/AppOverlay.react.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import Overlay from './Overlay.react';
+import './appOverlay.css';
 
-class AppOverlay extends React.Component {
-
-    static defaultProps = {
-        color: 'blue',
-        text: 'Confirm',
-    }
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <Overlay color="red" text="vincent" />
+function AppOverlay({
+    url = 'http://m.diandong.com/app/'
+}) {
+    return (
+        <div className="app-overlay">
+            <div className="wrap">
+                <div className="app-overlay-logo fn-left"></div>
+                <div className="app-overlay-text fn-left">
+                    <h3>电动邦</h3>
+                    <p>新能源汽车第一导购平台</p>
+                </div>
+                <a className="app-overlay-button fn-right" href={url}>下载APP</a>
+                <i className="app-overlay-star"></i>
             </div>
-        );
-    }
-};
+        </div>
+    );
+}
 
 export default AppOverlay;
